f02803c729fe1574a07f86e51bae1879231b8497
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-08-06 00:02:43 +0900

    implement basic bottom navigation bar

--- FooS/app/src/main/java/com/example/foos/MainActivity.kt
+++ FooS/app/src/main/java/com/example/foos/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.foos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.foos.ui.theme.FooSTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +32,71 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    val items = listOf(
+                        Screen.Home,
+                        Screen.Map
+                    )
+                    Scaffold(
+                        bottomBar = {
+                            BottomNavigation {
+                                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                                val currentDestination = navBackStackEntry?.destination
+                                items.forEach { screen ->
+                                    BottomNavigationItem(
+                                        icon = {
+                                            Icon(
+                                                Icons.Filled.Favorite,
+                                                contentDescription = null
+                                            )
+                                        },
+                                        label = { Text(stringResource(id = screen.resourceId)) },
+                                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                                        onClick = {
+                                            navController.navigate(screen.route) {
+                                                popUpTo(navController.graph.findStartDestination().id) {
+                                                    saveState = true
+                                                }
+                                                launchSingleTop = true
+                                                restoreState = true
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    ) { innerPadding ->
+                        NavHost(
+                            navController,
+                            startDestination = Screen.Home.route,
+                            Modifier.padding(innerPadding)
+                        ) {
+                            composable(Screen.Home.route) { Home(navController) }
+                            composable(Screen.Map.route) { Map(navController) }
+                        }
+                    }
                 }
             }
         }
     }
+
+}
+
+sealed class Screen(val route: String, @StringRes val resourceId: Int) {
+    object Home : Screen("home", R.string.home)
+    object Map : Screen("map", R.string.map)
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FooSTheme {
-        Greeting("Android")
+fun Home(navController: NavController) {
+    Button(onClick = { navController.navigate("map") { launchSingleTop = true } }) {
+        Text(text = "Navigate text")
     }
+
+}
+
+@Composable
+fun Map(navController: NavController) {
+
+    Text(text = "map")
 }
