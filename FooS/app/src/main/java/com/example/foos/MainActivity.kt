14b3906e482067202ec5c56db89be269fd50691d
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-08-06 11:11:05 +0900

    fix the crash of google map and implement basic google map view

--- FooS/app/src/main/java/com/example/foos/MainActivity.kt
+++ FooS/app/src/main/java/com/example/foos/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.foos
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +15,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -23,10 +31,42 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.foos.ui.theme.FooSTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    }
+
+    val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            // if permission is granted
+            locationPermissionGranted = true
+        } else {
+            // if permission is not granted
+
+        }
+    }
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    var locationPermissionGranted: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setupGoogleMap()
+        startLocationPermissionRequest()
         setContent {
             FooSTheme {
                 // A surface container using the 'background' color from the theme
@@ -34,55 +74,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val navController = rememberNavController()
-                    val items = listOf(
-                        Screen.Home,
-                        Screen.Map
-                    )
-                    Scaffold(
-                        bottomBar = {
-                            BottomNavigation {
-                                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                                val currentDestination = navBackStackEntry?.destination
-                                items.forEach { screen ->
-                                    BottomNavigationItem(
-                                        icon = {
-                                            Icon(
-                                                painter = painterResource(id = screen.iconId),
-                                                contentDescription = null
-                                            )
-                                        },
-                                        label = { Text(stringResource(id = screen.stringId)) },
-                                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                                        onClick = {
-                                            navController.navigate(screen.route) {
-                                                popUpTo(navController.graph.findStartDestination().id) {
-                                                    saveState = true
-                                                }
-                                                launchSingleTop = true
-                                                restoreState = true
-                                            }
-                                        }
-                                    )
-                                }
-                            }
-                        }
-                    ) { innerPadding ->
-                        NavHost(
-                            navController,
-                            startDestination = Screen.Home.route,
-                            Modifier.padding(innerPadding)
-                        ) {
-                            composable(Screen.Home.route) { Home(navController) }
-                            composable(Screen.Map.route) { Map(navController) }
-                        }
-                    }
+                    App()
                 }
             }
         }
     }
 
+    private fun setupGoogleMap() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(applicationContext)
+    }
+
+    fun startLocationPermissionRequest() {
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
 }
+
 
 sealed class Screen(val route: String, @StringRes val stringId: Int, @DrawableRes val iconId: Int) {
     object Home : Screen("home", R.string.home, R.drawable.ic_home)
@@ -90,15 +97,73 @@ sealed class Screen(val route: String, @StringRes val stringId: Int, @DrawableRe
 }
 
 @Composable
-fun Home(navController: NavController) {
-    Button(onClick = { navController.navigate("map") { launchSingleTop = true } }) {
-        Text(text = "Navigate text")
+fun App() {
+    val navController = rememberNavController()
+    val items = listOf(
+        Screen.Home,
+        Screen.Map
+    )
+    Scaffold(
+        bottomBar = {
+            BottomNavigation {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                items.forEach { screen ->
+                    BottomNavigationItem(
+                        icon = {
+                            Icon(
+                                painter = painterResource(id = screen.iconId),
+                                contentDescription = null
+                            )
+                        },
+                        label = { Text(stringResource(id = screen.stringId)) },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController,
+            startDestination = Screen.Home.route,
+            Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) { Home(navController) }
+            composable(Screen.Map.route) { Map(navController) }
+        }
     }
+}
 
+@Composable
+fun Home(navController: NavController) {
+    Text(text = "Navigate text")
 }
 
 @Composable
 fun Map(navController: NavController) {
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
 
-    Text(text = "map")
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
+    ) {
+        Marker(
+            state = MarkerState(position = singapore),
+            title = "Singapore",
+            snippet = "Marker in Singapore"
+        )
+    }
+
 }
