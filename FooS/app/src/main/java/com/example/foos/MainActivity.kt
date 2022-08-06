3ea18cabc4ee5e0d2ac7fb3be8dcb1a601a11d5a
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-08-06 12:10:10 +0900

    move code about managing location permission to map view itself

--- FooS/app/src/main/java/com/example/foos/MainActivity.kt
+++ FooS/app/src/main/java/com/example/foos/MainActivity.kt
@@ -1,28 +1,22 @@
 package com.example.foos
 
 import android.Manifest
-import android.content.pm.PackageManager
+import android.annotation.SuppressLint
 import android.location.Location
-import android.location.LocationManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -31,42 +25,20 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.foos.ui.theme.FooSTheme
-import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.rememberPermissionState
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.*
 
 
 class MainActivity : ComponentActivity() {
 
-    companion object {
-        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
-    }
-
-    val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (isGranted) {
-            // if permission is granted
-            locationPermissionGranted = true
-        } else {
-            // if permission is not granted
-
-        }
-    }
-
-    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    var locationPermissionGranted: Boolean = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setupGoogleMap()
-        startLocationPermissionRequest()
         setContent {
             FooSTheme {
                 // A surface container using the 'background' color from the theme
@@ -79,15 +51,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-    private fun setupGoogleMap() {
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(applicationContext)
-    }
-
-    fun startLocationPermissionRequest() {
-        requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-    }
-
 }
 
 
@@ -148,22 +111,49 @@ fun Home(navController: NavController) {
     Text(text = "Navigate text")
 }
 
+@SuppressLint("MissingPermission")
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun Map(navController: NavController) {
-    val singapore = LatLng(1.35, 103.87)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
-    }
 
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState,
-    ) {
-        Marker(
-            state = MarkerState(position = singapore),
-            title = "Singapore",
-            snippet = "Marker in Singapore"
-        )
-    }
+    val locationPermissionState = rememberPermissionState(permission = Manifest.permission.ACCESS_FINE_LOCATION)
 
+    when (locationPermissionState.status) {
+        PermissionStatus.Granted -> {
+            var latLng by remember { mutableStateOf(LatLng(1.35, 103.87)) }
+            val cameraPositionState = rememberCameraPositionState {
+                position = CameraPosition.fromLatLngZoom(latLng, 10f)
+            }
+
+            val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(LocalContext.current)
+
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location? ->
+                location?.let {
+                    latLng = LatLng(it.latitude, it.longitude)
+                    cameraPositionState.position = CameraPosition.fromLatLngZoom(latLng, 20f)
+                }
+            }
+
+            GoogleMap(
+                modifier = Modifier.fillMaxSize(),
+                cameraPositionState = cameraPositionState,
+            ) {
+                Marker(
+                    state = MarkerState(position = latLng),
+                    title = "CurrentLocation",
+                    snippet = "Marker of your location"
+                )
+            }
+        }
+        else -> {
+            Column {
+                Text(
+                    text = "Please give the permission."
+                )
+                Button(onClick = {locationPermissionState.launchPermissionRequest()} ) {
+                    Text("Request permission")
+                }
+            }
+        }
+    }
 }
