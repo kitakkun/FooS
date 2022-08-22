922208cc0050355393eb33adae2848f5f49fa82f
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-08-22 10:40:25 +0900

    Update AsyncUserIcon.kt

--- FooS/app/src/main/java/com/example/foos/ui/component/AsyncUserIcon.kt
+++ FooS/app/src/main/java/com/example/foos/ui/component/AsyncUserIcon.kt
@@ -1,20 +1,21 @@
 package com.example.foos.ui.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.example.foos.R
 
+/**
+ * ユーザーアイコン
+ */
 @Composable
 fun AsyncUserIcon(
     url: String,
@@ -22,13 +23,14 @@ fun AsyncUserIcon(
 ) {
     AsyncImage(
         model = ImageRequest.Builder(LocalContext.current)
-            .data(url).crossfade(true).build(), contentScale = ContentScale.Crop,
+            .data(url).crossfade(true)
+            .placeholder(R.drawable.ic_account_circle)
+            .build(),
+        contentScale = ContentScale.Crop,
         contentDescription = null,
         modifier = modifier
             .clip(CircleShape)
             .width(50.dp)
             .height(50.dp)
-            .background(Color.Gray)
-            .border(1.dp, Color.Gray)
     )
 }
