f0655fb8f2ee7d4a22ccfcbb61a6b27ab094dc2e
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-08-22 18:02:05 +0900

    add reaction button

--- FooS/app/src/main/java/com/example/foos/ui/view/screen/postdetail/PostDetailScreen.kt
+++ FooS/app/src/main/java/com/example/foos/ui/view/screen/postdetail/PostDetailScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -13,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,6 +54,8 @@ fun PostDetailScreen(viewModel: PostDetailViewModel, navController: NavControlle
             UserIcon(url = postItemUiState.userIcon)
             Spacer(Modifier.width(24.dp))
             UserIdentityColumn(username = postItemUiState.username, userId = postItemUiState.userId)
+            Spacer(Modifier.weight(1f))
+            ReactionButton()
         }
         Spacer(Modifier.height(24.dp))
         Text(postItemUiState.content)
@@ -128,5 +133,11 @@ fun AttachedImagesDisplay(
             )
         }
     }
+}
 
+@Composable
+fun ReactionButton() {
+    IconButton(onClick = { }) {
+        Icon(painterResource(R.drawable.ic_add_reaction), null)
+    }
 }
