45497153697c9c908493cae3522b6638e906f475
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-08-23 15:01:26 +0900

    refactor UsersRepository

--- FooS/app/src/main/java/com/example/foos/data/repository/UsersRepository.kt
+++ FooS/app/src/main/java/com/example/foos/data/repository/UsersRepository.kt
@@ -5,10 +5,18 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
 
+/**
+ * ユーザ情報を管理するリポジトリ
+ */
 object UsersRepository {
 
+    private const val COLLECTION = "users"
+
+    /**
+     * ユーザ情報を取得します
+     */
     suspend fun fetchUser(userId: String): User? {
-        val userData = Firebase.firestore.collection("users")
+        val userData = Firebase.firestore.collection(COLLECTION)
             .whereEqualTo("userId", userId)
             .get().await().toObjects(User::class.java)
         return if (userData.size > 0) {
@@ -18,4 +26,22 @@ object UsersRepository {
         }
     }
 
+    /**
+     * ユーザ情報を更新します
+     */
+    suspend fun update(user: User) {
+        val document = Firebase.firestore.document(user.userId)
+        val updates = hashMapOf<String, Any>(
+            "username" to user.username,
+            "profileImage" to user.profileImage
+        )
+        document.update(updates).await()
+    }
+
+    /**
+     * ユーザを削除します
+     */
+    suspend fun delete(userId: String) {
+        Firebase.firestore.document(userId).delete()
+    }
 }
