d456d14a232edad94603ab9d4a5faacf20ceb82d
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-08-30 16:19:58 +0900

    add initial camera position control to GoogleMap

--- FooS/app/src/main/java/com/example/foos/ui/view/screen/locationselect/LocationSelectScreen.kt
+++ FooS/app/src/main/java/com/example/foos/ui/view/screen/locationselect/LocationSelectScreen.kt
@@ -1,21 +1,30 @@
 package com.example.foos.ui.view.screen.locationselect
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Location
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.example.foos.R
 import com.example.foos.ui.view.screen.ScreenViewModel
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.*
 
+@SuppressLint("MissingPermission")
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun LocationSelectScreen(
     viewModel: LocationSelectViewModel,
@@ -37,6 +46,29 @@ fun LocationSelectScreen(
         }
     }
 
+    val properties by remember { mutableStateOf(MapProperties(isMyLocationEnabled = true)) }
+    val uiSettings by remember { mutableStateOf(MapUiSettings(myLocationButtonEnabled = true)) }
+
+    val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(
+        LocalContext.current
+    )
+
+    val permissionStatus =
+        rememberPermissionState(permission = Manifest.permission.ACCESS_FINE_LOCATION)
+    val cameraPositionState = rememberCameraPositionState()
+
+    LaunchedEffect(Unit) {
+        if (permissionStatus.status == PermissionStatus.Granted) {
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location ->
+                location?.let {
+                    val latLng = LatLng(it.latitude, it.longitude)
+                    cameraPositionState.position = CameraPosition.fromLatLngZoom(latLng, 15f)
+                }
+            }
+        }
+    }
+
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -57,6 +89,9 @@ fun LocationSelectScreen(
         }
     ) { innerPadding ->
         GoogleMap(
+            cameraPositionState = cameraPositionState,
+            properties = properties,
+            uiSettings = uiSettings,
             onMapClick = {
                 sharedViewModel.updatePostCreateSharedData(
                     location = it,
