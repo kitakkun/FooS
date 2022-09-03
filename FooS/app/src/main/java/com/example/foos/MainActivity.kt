19305e2260c6195bc29c0af1c3e40cb523e8057b
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-09-03 23:45:26 +0900

    move emoji initialization into MyApplication.kt

--- FooS/app/src/main/java/com/example/foos/MainActivity.kt
+++ FooS/app/src/main/java/com/example/foos/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.foos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.emoji2.bundled.BundledEmojiCompatConfig
-import androidx.emoji2.text.EmojiCompat
 import com.example.foos.ui.view.screen.MainScreen
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,9 +11,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val config = BundledEmojiCompatConfig(applicationContext)
-        EmojiCompat.init(config)
 
         setContent {
             MainScreen()
--- FooS/app/src/main/java/com/example/foos/MyApplication.kt
+++ FooS/app/src/main/java/com/example/foos/MyApplication.kt
@@ -1,7 +1,17 @@
 package com.example.foos
 
 import android.app.Application
+import androidx.emoji2.bundled.BundledEmojiCompatConfig
+import androidx.emoji2.text.EmojiCompat
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplication : Application()
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val config = BundledEmojiCompatConfig(applicationContext)
+        EmojiCompat.init(config)
+    }
+    
+}
