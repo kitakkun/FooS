8952e650f61e15147bbcf70ac12b386258768aeb
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-09-04 21:19:15 +0900

    add FollowButtonPreview

--- FooS/app/src/main/java/com/example/foos/ui/view/component/FollowButton.kt
+++ FooS/app/src/main/java/com/example/foos/ui/view/component/FollowButton.kt
@@ -2,13 +2,21 @@ package com.example.foos.ui.view.component
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.foos.R
 
+/**
+ * フォローボタン
+ * @param following フォロー状態
+ * @param onClick ボタンクリック時のイベント
+ */
 @Composable
 fun FollowButton(
-    onClick: () -> Unit,
     following: Boolean,
+    onClick: () -> Unit = {},
 ) {
     val text =
         if (following) stringResource(id = R.string.following)
@@ -16,5 +24,16 @@ fun FollowButton(
     RoundButton(onClick = onClick, outlined = following) {
         Text(text = text)
     }
+}
 
+@Preview
+@Composable
+private fun FollowButtonPreview() {
+    val followingFlag = remember {
+        mutableStateOf(false)
+    }
+    FollowButton(
+        following = followingFlag.value,
+        onClick = { followingFlag.value = !followingFlag.value }
+    )
 }
