685e98b6b67740688774a322319b7e836dd8d3a7
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-09-04 21:34:13 +0900

    add UserIconPreview

--- FooS/app/src/main/java/com/example/foos/ui/view/component/UserIcon.kt
+++ FooS/app/src/main/java/com/example/foos/ui/view/component/UserIcon.kt
@@ -9,9 +9,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import com.example.foos.R
 
@@ -39,6 +39,12 @@ fun UserIcon(
             .clip(CircleShape)
             .width(50.dp)
             .height(50.dp)
-            .clickable { onClick.invoke() }
+            .clickable { onClick() }
     )
+}
+
+@Preview
+@Composable
+private fun UserIconPreview() {
+    UserIcon(url = "")
 }
