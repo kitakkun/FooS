ced8e3ae98ae79707928525270d2b7ae792121e0
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-09-12 17:33:01 +0900

    remove unnecessary composable functions

--- FooS/app/src/main/java/com/example/foos/SignInActivity.kt
+++ FooS/app/src/main/java/com/example/foos/SignInActivity.kt
@@ -80,31 +80,7 @@ class SignInActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FooSTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Button(onClick = {
-
-                    }) {
-                        Greeting("Android")
-                    }
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FooSTheme {
-        Greeting("Android")
     }
 }
