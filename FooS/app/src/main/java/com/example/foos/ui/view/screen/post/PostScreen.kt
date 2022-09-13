864386b3f4d050302bf2e349afbb0101dd9c310d
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-09-13 12:12:16 +0900

    fix weird format of code

--- FooS/app/src/main/java/com/example/foos/ui/view/screen/post/PostScreen.kt
+++ FooS/app/src/main/java/com/example/foos/ui/view/screen/post/PostScreen.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -15,16 +14,11 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.foos.R
 import com.example.foos.ui.constants.paddingMedium
@@ -76,8 +70,7 @@ fun PostScreen(
 
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetMultipleContents()
-    )
-    {
+    ) {
         viewModel.setImages(context, it)
     }
 
@@ -130,7 +123,7 @@ fun PostScreen(
             Attachments(
                 attachedImages = uiState.attachedImages,
                 locationAttached = locationAttached,
-                onImageAttachmentRemove = { viewModel.onImageAttachmentRemove(it)},
+                onImageAttachmentRemove = { viewModel.onImageAttachmentRemove(it) },
                 onLocationRemove = {
                     viewModel.onLocationRemove()
                     sharedViewModel.updatePostCreateSharedData(null, null)
@@ -177,7 +170,7 @@ fun Attachments(
         }
         if (locationAttached) {
             item {
-                LocationAttachment (onCloseButtonClick = onLocationRemove)
+                LocationAttachment(onCloseButtonClick = onLocationRemove)
             }
         }
     }
