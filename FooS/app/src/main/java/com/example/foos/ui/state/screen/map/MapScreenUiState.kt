da14f2f0bce81215b6ce0164720722e63bff2725
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-09-16 13:10:21 +0900

    create MapUIPreview

--- FooS/app/src/main/java/com/example/foos/ui/state/screen/map/MapScreenUiState.kt
+++ FooS/app/src/main/java/com/example/foos/ui/state/screen/map/MapScreenUiState.kt
@@ -5,4 +5,11 @@ import com.example.foos.ui.state.component.PostItemUiState
 data class MapScreenUiState(
     val posts: List<PostItemUiState>,
     val focusingPost: PostItemUiState,
-)
+) {
+    companion object {
+        val Default = MapScreenUiState(
+            posts = listOf(),
+            focusingPost = PostItemUiState.Default
+        )
+    }
+}
