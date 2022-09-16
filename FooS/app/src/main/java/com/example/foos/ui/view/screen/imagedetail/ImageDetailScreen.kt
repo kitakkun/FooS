056dc06778032e9b699a10f8010b9d66e5e990c8
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-09-16 12:45:13 +0900

    create ImageDetailUIPreview, also add TopAppBar

--- FooS/app/src/main/java/com/example/foos/ui/view/screen/imagedetail/ImageDetailScreen.kt
+++ FooS/app/src/main/java/com/example/foos/ui/view/screen/imagedetail/ImageDetailScreen.kt
@@ -4,18 +4,24 @@ import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
-import coil.compose.AsyncImage
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
-import com.example.foos.R
 import com.example.foos.ui.view.component.MaxSizeLoadingIndicator
 import dev.chrisbanes.snapper.ExperimentalSnapperApi
 import dev.chrisbanes.snapper.rememberSnapperFlingBehavior
@@ -25,7 +31,6 @@ import kotlinx.coroutines.launch
  * 画像を全画面プレビューするスクリーン
  * 投稿内容の画像をタップした時などに遷移
  */
-@OptIn(ExperimentalSnapperApi::class)
 @Composable
 fun ImageDetailScreen(
     navController: NavHostController,
@@ -44,15 +49,40 @@ fun ImageDetailScreen(
             }
         }
     }
-    LazyRow(
-        state = lazyListState,
-        flingBehavior = rememberSnapperFlingBehavior(lazyListState),
-        modifier = Modifier.fillMaxSize(),
-    )
-    {
-        items(imageUrls) {
-            Log.d("TAG", it)
-            FullSizeImage(url = it, modifier = Modifier.fillParentMaxSize())
+
+    ImageDetailUI(
+        lazyListState = lazyListState,
+        imageUrls = imageUrls,
+        onBack = { navController.navigateUp() })
+}
+
+@OptIn(ExperimentalSnapperApi::class)
+@Composable
+private fun ImageDetailUI(
+    lazyListState: LazyListState,
+    imageUrls: List<String>,
+    onBack: () -> Unit
+) {
+    Box {
+        TopAppBar(
+            title = {},
+            navigationIcon = {
+                IconButton(onClick = onBack) {
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+                }
+            },
+            backgroundColor = Color.Transparent.copy(alpha = 0.1f),
+        )
+        LazyRow(
+            state = lazyListState,
+            flingBehavior = rememberSnapperFlingBehavior(lazyListState),
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            items(imageUrls) {
+                Log.d("TAG", it)
+                FullSizeImage(url = it, modifier = Modifier.fillParentMaxSize())
+            }
         }
     }
 }
@@ -63,7 +93,7 @@ fun ImageDetailScreen(
  * @param modifier モディファイア
  */
 @Composable
-fun FullSizeImage(
+private fun FullSizeImage(
     url: String,
     modifier: Modifier = Modifier
 ) {
@@ -83,4 +113,10 @@ fun FullSizeImage(
             modifier = Modifier.fillMaxWidth()
         )
     }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun ImageDetailUIPreview() {
+    ImageDetailUI(lazyListState = rememberLazyListState(), imageUrls = listOf(), onBack = {})
 }
