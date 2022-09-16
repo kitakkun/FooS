d1ad1d3cc2008af7c98b0425a42b8adc2f3d7fc9
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-09-16 12:55:18 +0900

    fix a problem that backArrow icon is difficult to see

--- FooS/app/src/main/java/com/example/foos/ui/view/screen/imagedetail/ImageDetailScreen.kt
+++ FooS/app/src/main/java/com/example/foos/ui/view/screen/imagedetail/ImageDetailScreen.kt
@@ -1,6 +1,6 @@
 package com.example.foos.ui.view.screen.imagedetail
 
-import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,14 +8,17 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
+import com.example.foos.ui.theme.FooSTheme
 import com.example.foos.ui.view.component.MaxSizeLoadingIndicator
 import dev.chrisbanes.snapper.ExperimentalSnapperApi
 import dev.chrisbanes.snapper.rememberSnapperFlingBehavior
@@ -72,15 +76,22 @@ private fun ImageDetailUI(
                 .fillMaxSize()
         ) {
             items(imageUrls) {
-                Log.d("TAG", it)
                 FullSizeImage(url = it, modifier = Modifier.fillParentMaxSize())
             }
         }
         TopAppBar(
             title = {},
             navigationIcon = {
-                IconButton(onClick = onBack) {
-                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+                IconButton(
+                    onClick = onBack,
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colors.secondary.copy(alpha = 0.4f))
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack, contentDescription = null,
+                        tint = MaterialTheme.colors.onSecondary
+                    )
                 }
             },
             backgroundColor = Color.Transparent.copy(alpha = 0f),
@@ -120,5 +131,7 @@ private fun FullSizeImage(
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
 private fun ImageDetailUIPreview() {
-    ImageDetailUI(lazyListState = rememberLazyListState(), imageUrls = listOf(), onBack = {})
+    FooSTheme {
+        ImageDetailUI(lazyListState = rememberLazyListState(), imageUrls = listOf(), onBack = {})
+    }
 }
