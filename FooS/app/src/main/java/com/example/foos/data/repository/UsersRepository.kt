c57f47cbe18e6a8c614590511f4654ac37143081
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-09-26 15:25:57 +0900

    add registering process when first login

--- FooS/app/src/main/java/com/example/foos/data/repository/UsersRepository.kt
+++ FooS/app/src/main/java/com/example/foos/data/repository/UsersRepository.kt
@@ -3,6 +3,7 @@ package com.example.foos.data.repository
 import com.example.foos.data.model.database.DatabaseUser
 
 interface UsersRepository {
+    suspend fun create(databaseUser: DatabaseUser)
     suspend fun fetchByUserId(userId: String): DatabaseUser?
     suspend fun fetchByUserIds(userIds: List<String>): List<DatabaseUser>
     suspend fun update(databaseUser: DatabaseUser)
