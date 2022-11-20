ee56f9f7be07b69ac23fb398dad03371cf3e8f16
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-11-20 13:27:53 +0900

    split into Screen and Ui to preview it

--- app/src/main/java/com/example/foos/ui/view/screen/login_screen/LoginScreen.kt
+++ app/src/main/java/com/example/foos/ui/view/screen/login_screen/LoginScreen.kt
@@ -1,25 +1,7 @@
 package com.example.foos.ui.view.screen.login_screen
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -28,75 +10,11 @@ fun LoginScreen(
 ) {
     val uiState by viewModel.uiState
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .fillMaxSize(),
-    ) {
-        Text(text = "Welcome to FooS!", style = MaterialTheme.typography.h4)
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        TextField(
-            value = uiState.email,
-            singleLine = true,
-            onValueChange = viewModel::onEmailChange,
-            label = { Text(text = "email") },
-            trailingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Email,
-                    contentDescription = null
-                )
-            },
-            keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Next,
-                keyboardType = KeyboardType.Email
-            )
-        )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Row {
-            TextField(
-                value = uiState.password,
-                visualTransformation = if (uiState.isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                singleLine = true,
-                onValueChange = viewModel::onPasswordChange,
-                label = { Text(text = "password") },
-                trailingIcon = {
-                    Icon(
-                        imageVector = if (uiState.isPasswordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
-                        contentDescription = null,
-                        modifier = Modifier.clickable { viewModel::onPasswordVisibilityChange.invoke(!uiState.isPasswordVisible) }
-                    )
-                },
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Done,
-                    keyboardType = KeyboardType.Password
-                )
-            )
-        }
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Button(onClick = viewModel::onCreateAccountClicked) {
-            Text(text = "Create account")
-        }
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Row {
-            Text(text = "Have an account?")
-            ClickableText(text = buildAnnotatedString {
-                append("Log in")
-            }, onClick = {})
-        }
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-private fun LoginScreenPreview() {
-    LoginScreen()
+    LoginUi(
+        uiState = uiState,
+        onCreateAccountClicked = viewModel::onCreateAccountClicked,
+        onEmailChange = viewModel::onEmailChange,
+        onPasswordChange = viewModel::onPasswordChange,
+        onPasswordVisibilityChange = viewModel::onPasswordVisibilityChange
+    )
 }
