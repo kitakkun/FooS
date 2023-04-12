96ec2123df2ec710d803ca91c680328fdf912502
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-04-12 18:47:09 +0900

    メールとパスワード用テキストフィールド追加

---
+++ app/src/main/java/com/example/foos/ui/view/component/EmailTextField.kt
@@ -0,0 +1,55 @@
+package com.example.foos.ui.view.component
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.foos.R
+
+@Composable
+fun EmailTextField(
+    email: String,
+    onEmailChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    OutlinedTextField(
+        value = email,
+        onValueChange = onEmailChange,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Email,
+            imeAction = ImeAction.Next,
+        ),
+        label = { Text(text = stringResource(id = R.string.email)) },
+        placeholder = { Text(text = stringResource(id = R.string.example_email)) },
+        trailingIcon = {
+            Icon(imageVector = Icons.Default.Email, contentDescription = null)
+        },
+        modifier = modifier,
+    )
+}
+
+@Preview
+@Composable
+private fun EmptyEmailTextFieldPreview() {
+    EmailTextField(
+        email = "",
+        onEmailChange = {},
+    )
+}
+
+@Preview
+@Composable
+private fun NonEmptyEmailTextFieldPreview() {
+    EmailTextField(
+        email = "example@example.com",
+        onEmailChange = {},
+    )
+}
