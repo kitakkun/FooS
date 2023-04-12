dc52d90270ee07d331c2aee0d6fe4b8f446f6309
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-04-12 19:53:36 +0900

    userモジュールのパッケージが間違っていたので修正

--- user/src/androidTest/java/com/github/kitakkun/foos/user/ExampleInstrumentedTest.kt
+++ user/src/androidTest/java/com/github/kitakkun/foos/user/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
-package com.github.kitakkun.user
+package com.github.kitakkun.foos.user
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.github.kitakkun.user.test", appContext.packageName)
+        assertEquals("com.github.kitakkun.foos.user.test", appContext.packageName)
     }
 }
--- user/src/test/java/com/github/kitakkun/foos/user/ExampleUnitTest.kt
+++ user/src/test/java/com/github/kitakkun/foos/user/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package com.github.kitakkun.user
+package com.github.kitakkun.foos.user
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
