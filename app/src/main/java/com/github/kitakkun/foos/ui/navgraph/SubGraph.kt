e181eb8a8892a71eca0b87ec77b30e9e8492c010
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-04-17 11:27:10 +0900

    投稿作成のGraphを別ファイルへ切り出し

--- app/src/main/java/com/github/kitakkun/foos/ui/navgraph/SubGraph.kt
+++ app/src/main/java/com/github/kitakkun/foos/ui/navgraph/SubGraph.kt
@@ -4,16 +4,10 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import androidx.navigation.navigation
 import com.github.kitakkun.foos.common.ScreenViewModel
 import com.github.kitakkun.foos.common.navigation.StringList
 import com.github.kitakkun.foos.common.navigation.SubScreen
-import com.github.kitakkun.foos.post.create.PostScreen
-import com.github.kitakkun.foos.post.create.PostViewModelImpl
-import com.github.kitakkun.foos.post.create.locationconfirm.LocationConfirmScreen
-import com.github.kitakkun.foos.post.create.locationconfirm.LocationConfirmViewModelImpl
-import com.github.kitakkun.foos.post.create.locationselect.LocationSelectScreen
-import com.github.kitakkun.foos.post.create.locationselect.LocationSelectViewModelImpl
+import com.github.kitakkun.foos.post.create.postCreateGraph
 import com.github.kitakkun.foos.post.imagedetail.ImageDetailScreen
 import com.github.kitakkun.foos.post.postdetail.PostDetailScreen
 import com.github.kitakkun.foos.post.postdetail.PostDetailViewModelImpl
@@ -26,23 +20,7 @@ fun NavGraphBuilder.subGraph(
     navController: NavController,
     screenViewModel: ScreenViewModel,
 ) {
-    navigation(
-        route = SubScreen.PostCreate.route,
-        startDestination = SubScreen.PostCreate.Edit.route,
-    ) {
-        composable(SubScreen.PostCreate.Edit.route) {
-            val vm: PostViewModelImpl = hiltViewModel()
-            PostScreen(vm, navController, screenViewModel)
-        }
-        composable(SubScreen.PostCreate.LocationSelect.route) {
-            val vm: LocationSelectViewModelImpl = hiltViewModel()
-            LocationSelectScreen(vm, navController, screenViewModel)
-        }
-        composable(SubScreen.PostCreate.LocationConfirm.route) {
-            val vm: LocationConfirmViewModelImpl = hiltViewModel()
-            LocationConfirmScreen(vm, navController, screenViewModel)
-        }
-    }
+    postCreateGraph(navController, screenViewModel)
     composable(
         SubScreen.UserProfile.routeWithParam,
         SubScreen.UserProfile.arguments,
