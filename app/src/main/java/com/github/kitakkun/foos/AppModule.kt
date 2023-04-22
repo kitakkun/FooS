18f108716086f64b591b58d8e0638b72e792a8d1
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-04-22 09:52:27 +0900

    MapViewModelのkoin定義

--- app/src/main/java/com/github/kitakkun/foos/AppModule.kt
+++ app/src/main/java/com/github/kitakkun/foos/AppModule.kt
@@ -3,7 +3,9 @@ package com.github.kitakkun.foos
 import com.github.kitakkun.foos.common.di.firebaseModule
 import com.github.kitakkun.foos.common.di.repositoryModule
 import com.github.kitakkun.foos.common.di.useCaseModule
+import com.github.kitakkun.foos.ui.screen.map.MapViewModel
 import com.github.kitakkun.foos.user.userModule
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
@@ -13,4 +15,7 @@ val appModule = module {
         userModule,
         useCaseModule,
     )
+
+    // TODO: 後で違うモジュールに移動したい
+    viewModel { MapViewModel(get()) }
 }
