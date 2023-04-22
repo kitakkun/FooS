6553641cf83638b62f05417f41abe0f3543b2aa5
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-04-22 10:15:02 +0900

    postModuleを読み込む

--- app/src/main/java/com/github/kitakkun/foos/AppModule.kt
+++ app/src/main/java/com/github/kitakkun/foos/AppModule.kt
@@ -3,6 +3,7 @@ package com.github.kitakkun.foos
 import com.github.kitakkun.foos.common.di.firebaseModule
 import com.github.kitakkun.foos.common.di.repositoryModule
 import com.github.kitakkun.foos.common.di.useCaseModule
+import com.github.kitakkun.foos.post.postModule
 import com.github.kitakkun.foos.ui.screen.map.MapViewModel
 import com.github.kitakkun.foos.user.userModule
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -14,6 +15,7 @@ val appModule = module {
         repositoryModule,
         userModule,
         useCaseModule,
+        postModule,
     )
 
     // TODO: 後で違うモジュールに移動したい
