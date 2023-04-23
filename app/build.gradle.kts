396aa7f4b61c9fd4c24939c2cadc926b567772f3
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-04-23 22:30:40 +0900

    appモジュールのgradleをktsにする

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.android.application")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("com.google.gms.google-services") // Google Services plugin
+}
+
+apply { from(rootProject.file("gradle/project_common.gradle").absolutePath) }
+
+android {
+    defaultConfig {
+        applicationId = "com.github.kitakkun.foos"
+    }
+}
+
+dependencies {
+    implementation(project(":user"))
+    implementation(project(":customview"))
+    implementation(project(":common"))
+    implementation(project(":post"))
+}
